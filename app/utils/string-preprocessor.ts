45be8318823f5732bb5c6eeec5c7b209d6c8b308
vblong <vu09@ads.uni-passau.de>
2021-03-04 22:09:30 +0100

    Escape some character again

--- app/utils/string-preprocessor.ts
+++ app/utils/string-preprocessor.ts
@@ -5,6 +5,11 @@ export function preprocess(input: string) {
 }
 
 export function correctString(input: string) {
+    /**
+     * Escape all strange characters
+     */
+    input = input.split("'").join("''");
+    input = input.split('"').join('""');
     
     return input;
 }
