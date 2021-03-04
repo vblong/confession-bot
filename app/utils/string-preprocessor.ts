0b4cbc0ed9e883fd4d6f6a2b929ec87e1b62e28e
vblong <vu09@ads.uni-passau.de>
2021-03-04 21:59:14 +0100

    No need to escape string

--- app/utils/string-preprocessor.ts
+++ app/utils/string-preprocessor.ts
@@ -5,13 +5,6 @@ export function preprocess(input: string) {
 }
 
 export function correctString(input: string) {
-    /**
-     * Escape all strange characters
-     */
-    input = input.split("'").join(":single_quote:");
-    input = input.split('"').join(":double_quote:");
-    input = input.split("(").join(":open_bracket:");
-    input = input.split(")").join(":close_bracket:");
-
+    
     return input;
 }
